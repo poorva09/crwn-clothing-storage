429627e601359e8fd1e83919a08443c65e8ce3b5
barwads <mathanman123>
2020-04-15 14:07:27 +0530

    Updates

---
+++ precache-manifest.0839bae29f32b8f8c3fd68c747d27422.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6b89d4ff38a5d928b5a17bd11b15dd",
+    "url": "/poorva09/crwn-clothing-storage/index.html"
+  },
+  {
+    "revision": "ea14cb105bd1d5d2a0b4",
+    "url": "/poorva09/crwn-clothing-storage/static/css/main.e105746c.chunk.css"
+  },
+  {
+    "revision": "3a731c3aa4196be84726",
+    "url": "/poorva09/crwn-clothing-storage/static/js/2.c3e7dd78.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/poorva09/crwn-clothing-storage/static/js/2.c3e7dd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea14cb105bd1d5d2a0b4",
+    "url": "/poorva09/crwn-clothing-storage/static/js/main.b6af7805.chunk.js"
+  },
+  {
+    "revision": "f1490cfa1fdc5d357451",
+    "url": "/poorva09/crwn-clothing-storage/static/js/runtime-main.af10dc3b.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/poorva09/crwn-clothing-storage/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/poorva09/crwn-clothing-storage/static/media/shopping-bag.30f07071.svg"
+  }
+]);
